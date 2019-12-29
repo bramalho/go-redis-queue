3c35dba4e44ad3b3a23325d6384b56971e1db051
Bruno Ramalho <bfmramalho@gmail.com>
2019-12-29 18:43:08 +0100

    redis queue

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go-redis-queue
 
 go 1.13
+
+require (
+	github.com/adjust/redismq v0.0.0-20170113163246-e2a56d9bb404
+	github.com/joho/godotenv v1.3.0
+	gopkg.in/bsm/ratelimit.v1 v1.0.0-20160220154919-db14e161995a // indirect
+	gopkg.in/redis.v3 v3.6.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/adjust/redismq"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("It works!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+	redisPass := os.Getenv("REDIS_PASS")
+	redisDB, _ := strconv.ParseInt(os.Getenv("REDIS_DB"), 10, 64)
+	redisQueue := os.Getenv("REDIS_QUEUE")
+
+	queue := redismq.CreateQueue(redisHost, redisPort, redisPass, redisDB, redisQueue)
+	for i := 0; i < 0; i++ {
+		queue.Put("Payload " + strconv.Itoa(i))
+	}
+
+	consumer, err := queue.AddConsumer("my_consumer")
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		p, err := consumer.Get()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println("Package " + strconv.Itoa(i))
+		fmt.Println(p.CreatedAt)
+		fmt.Println(p.Payload)
+
+		err = p.Ack()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
